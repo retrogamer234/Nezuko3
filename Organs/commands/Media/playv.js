5eec51de46eb15d717028cc62388c556eba5483b
retrogamer234 <101521533+retrogamer234@users.noreply.github.com>
2023-06-26 22:40:15 +0530

    Update playv.js

--- Organs/commands/Media/playv.js
+++ Organs/commands/Media/playv.js
@@ -30,7 +30,7 @@ await client.sendMessage(m.from,{
     contextInfo:{
         externalAdReply:{
             title:anu.title,
-            body: "©Eternity-Team",
+            body: "ɪᴢᴜᴍɪ",
             thumbnail: await fetchBuffer(anu.thumbnail),
             mediaType:2,
             mediaUrl:anu.url,
