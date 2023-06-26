e23435d9d6a4d9a56cbb7ec16bb6f25c1cf6d557
retrogamer234 <101521533+retrogamer234@users.noreply.github.com>
2023-06-26 22:58:16 +0530

    Update playv.js

--- Organs/commands/Media/playv.js
+++ Organs/commands/Media/playv.js
@@ -30,7 +30,7 @@ await client.sendMessage(m.from,{
     contextInfo:{
         externalAdReply:{
             title:anu.title,
-            body: "ɪᴢᴜᴍɪ",
+            body: "Ｍｉｔｓｕ",
             thumbnail: await fetchBuffer(anu.thumbnail),
             mediaType:2,
             mediaUrl:anu.url,
